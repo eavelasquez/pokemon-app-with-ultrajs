96f7f4b8d4afcdd5e6b0c06a10e92dc28eff52f9
Esteban Velasquez <eavc.30@gmail.com>
2022-05-26 13:30:28 -0500

    feat(front): add Pokemon component

---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type MovesType = {
+  move: { name: string; url: string };
+  version_group_details: [];
+};
+
+type PokemonPropType = {
+  name: string;
+  height: number;
+  weight: number;
+  xp: number;
+  image: string;
+  moves: Array<MovesType>;
+};
+
+const renderMoves = (moves: Array<MovesType>) => {
+  return moves.slice(0, 5).map(({ move }: MovesType) => {
+    return <li>{move?.name}</li>;
+  });
+};
+
+const Pokemon = ({ name, height, weight, image, moves }: PokemonPropType) => {
+  return (
+    <div className="card" style={{ width: "40rem" }}>
+      <img className="card-img-top" src={image} alt={name} />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">
+          Height: {height} | Weight: {weight}
+        </h6>
+        <p className="card-text">
+          <ul>{renderMoves(moves)}</ul>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
