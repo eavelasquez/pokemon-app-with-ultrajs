e9583a2ac6ea601169d9001f19145588ac6423f5
Esteban Velasquez <eavc.30@gmail.com>
2022-05-26 14:52:00 -0500

    feat(front): add index files for pages and components

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as Alert } from "./Alert.tsx";
+export { default as Berries } from "./Berries.tsx";
+export { default as List } from "./List.tsx";
+export { default as Navigation } from "./Navigation.tsx";
+export { default as Pokemon } from "./Pokemon.tsx";
