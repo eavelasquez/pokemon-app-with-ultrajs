b194dd14774dde72ec3d1a7c783e69d7debd3a2a
Esteban Velasquez <eavc.30@gmail.com>
2022-05-26 14:58:51 -0500

    refac(front): update Selected page
    - add tsx extension to each import.
    - add useEffect hook.
    - change class for columns.

--- src/pages/Selected.tsx
+++ src/pages/Selected.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment, useEffect, useState } from "react";
 import useSWR from "swr";
 
-import Alert from "../components/Alert";
-import Pokemon from "../components/Pokemon";
+import Alert from "../components/Alert.tsx";
+import Pokemon from "../components/Pokemon.tsx";
 
 const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
@@ -13,10 +13,14 @@ const Selected = ({ params }: SelectedProps) => {
 
   const { data, error } = useSWR(
     mounted && params?.id
-      ? `https://pokeapi.co/api/v2/pokemon/${params.id}`
+      ? `https://pokeapi.co/api/v2/pokemon/${params?.id}`
       : null,
     fetcher
   );
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   if (error) {
     return <Alert message="Unable to fetch data from pokemon API" />;
@@ -26,15 +30,15 @@ const Selected = ({ params }: SelectedProps) => {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-5"></div>
+          <div className="col-md-5"></div>
 
-          <div className="col-2">
+          <div className="col-md-2">
             <div className="spinner-border text-primary" role="status">
               <span className="sr-only">Loading...</span>
             </div>
           </div>
 
-          <div className="col-5"></div>
+          <div className="col-md-5"></div>
         </div>
       </div>
     );
@@ -43,20 +47,20 @@ const Selected = ({ params }: SelectedProps) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-1"></div>
+        <div className="col-md-1"></div>
 
-        <div className="col-10">
+        <div className="col-md-10">
           <Pokemon
             name={data?.name}
             height={data?.height}
             weight={data?.weight}
             xp={data?.base_experience}
-            image={data?.sprites?.front_default}
+            image={data?.sprites?.front_shiny}
             moves={data?.moves}
           />
         </div>
 
-        <div className="col-1"></div>
+        <div className="col-md-1"></div>
       </div>
     </div>
   );
